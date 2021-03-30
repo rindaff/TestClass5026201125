c6114e681bbdf69d653750ab351b76deef6f8834
rindaff <rindaf1@gmail.com>
2021-03-30 19:10:57 +0700

    TestClass5026201125

---
+++ TestClass5026201125.java
@@ -0,0 +1,13 @@
+ public class TestClass5026201125 {
+
+    public static void main(String args[]) {
+        ComputeMethods5026201125 fTC = new ComputeMethods5026201125();
+        System.out.println(fTC.fToC(50F));
+
+        ComputeMethods5026201125 Hypo = new ComputeMethods5026201125();
+        System.out.println(Hypo.hypotenuse(5, 9));
+
+        ComputeMethods5026201125 Dice = new ComputeMethods5026201125();
+        System.out.println(Dice.roll());
+    }
+}
